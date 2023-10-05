85e7eaf3e9e70e8b59012d9cdbca7c9a2d674148
tokslaw7 <70610570+tokslaw7@users.noreply.github.com>
2023-10-04 20:11:16 -0400

    Add files via upload

---
+++ app/data.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    [
+        'title' => 'Laravel Senior Developer',
+        'tags' => 'laravel,Javascript,PHP',
+        'company' => 'Acme Corp',
+        'location' => 'Boston,MA',
+        'email' => 'email@email.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry '
+    ],
+    [
+        'title' => 'Full Stack Engineer',
+        'tags' => 'laravel,Javascript,PHP',
+        'company' => 'Acme Corp',
+        'location' => 'Boston,MA',
+        'email' => 'email@email.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry '
+    ],
+    [
+        'title' => 'Laravel Senior Developer',
+        'tags' => 'laravel,Javascript,PHP',
+        'company' => 'Acme Corp',
+        'location' => 'Boston,MA',
+        'email' => 'email@email.com',
+        'website' => 'https://www.acme.com',
+        'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry '
+    ],
+];
